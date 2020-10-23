55c12570a3f4bbc87076e83f1944222be284bf3f
felipereyel <felipe.reyel@gmail.com>
2020-10-22 23:11:04 -0300

    add model and light refactor

--- index.js
+++ index.js
@@ -2,29 +2,45 @@ require("dotenv").config()
 const { driver } = require('@rocket.chat/sdk');
 const respmap  = require('./reply');
 
-const BOTNAME = process.env.BOTNAME;
-const ROOMS = [process.env.ROCKETCHAT_ROOM];
-
 var myuserid;
 
-const runbot = async () => {
+async function runbot () {
     await driver.connect()
     myuserid = await driver.login();
     await driver.subscribeToMessages();
-    await driver.reactToMessages( processDMs );
+    await driver.reactToMessages( processMessages );
 
-    await driver.sendToRoom(`${BOTNAME} is listening ...`, ROOMS[0]);
+    await driver.sendToRoom(
+      `@${process.env.ROCKETCHAT_USER} is listening ...`, 
+      process.env.GREETING_ROOM
+    );
 }
 
-const processDMs = async (err, message, messageOptions) => {
-  if (!err) {
-    if (message.u._id === myuserid) return;
+async function processMessages(err, message, messageOptions) {
+  if (err || message.u._id === myuserid) return;
 
-    const roomname = await driver.getRoomName(message.rid);
-
-    const response = respmap[message.msg] ?? `${message.u.username}, sorry didnt get that`;
-    await driver.sendToRoom(response, roomname);
+  switch (messageOptions.roomType) {
+    case "c":
+      return respondToChannel(message, messageOptions);
+    case "d":
+      return respondToDM(message, messageOptions);
+    default:
+      return;
   }
 }
 
-runbot()
+async function respondToChannel(message, messageOptions) {
+  if (process.env.RESPOND_TO_CHANNEL !== "true") return;
+
+  const response = respmap[message.msg] ?? `@${message.u.username}, sorry didnt get that`;
+  await driver.sendToRoomId(response, message.rid);
+}
+
+async function respondToDM(message, messageOptions) {
+  if (process.env.RESPOND_TO_DM !== "true") return;
+
+  const response = respmap[message.msg] ?? `@${message.u.username}, sorry didnt get that`;
+  await driver.sendDirectToUser(response, message.u.username);
+}
+
+runbot();
