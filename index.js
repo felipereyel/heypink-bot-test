8c2cfdb8ce65c8febfc89539973ce86c91bfa3dd
felipereyel <felipe.reyel@gmail.com>
2020-10-22 22:33:22 -0300

    working bot: direct sdk

--- index.js
+++ index.js
@@ -1,54 +1,29 @@
-require("dotenv").config();
+require("dotenv").config()
 const { driver } = require('@rocket.chat/sdk');
 const respmap  = require('./reply');
 
-// customize the following with your server and BOT account information
-const HOST = process.env.URL;
-const BOTNAME = process.env.BOT;  // name  bot response to
-const ROOMS = [process.env.ROOMS];
+const BOTNAME = process.env.BOTNAME;
+const ROOMS = [process.env.ROCKETCHAT_ROOM];
 
 var myuserid;
-// this simple bot does not handle errors, different messsage types, server resets 
-// and other production situations 
 
 const runbot = async () => {
-    const conn = await driver.connect( { host: HOST, useSsl: false})
-    myuserid = await driver.login({username: "abstrabot", password: "abstra123"});
-    const roomsJoined = await driver.joinRooms(ROOMS);
-    console.log('joined rooms');
+    await driver.connect()
+    myuserid = await driver.login();
+    await driver.subscribeToMessages();
+    await driver.reactToMessages( processDMs );
 
-    // set up subscriptions - rooms we are interested in listening to
-    const subscribed = await driver.subscribeToMessages();
-    console.log('subscribed');
-
-    // connect the processMessages callback
-    const msgloop = await driver.reactToMessages( processMessages );
-    console.log('connected and waiting for messages');
-
-    // when a message is created in one of the ROOMS, we 
-    // receive it in the processMesssages callback
-
-    // greets from the first room in ROOMS 
-    const sent = await driver.sendToRoom( BOTNAME + ' is listening ...',ROOMS[0]);
-    console.log('Greeting message sent');
+    await driver.sendToRoom(`${BOTNAME} is listening ...`, ROOMS[0]);
 }
 
-// callback for incoming messages filter and processing
-const processMessages = async(err, message, messageOptions) => {
+const processDMs = async (err, message, messageOptions) => {
   if (!err) {
-    // filter our own message
     if (message.u._id === myuserid) return;
-    // can filter further based on message.rid
+
     const roomname = await driver.getRoomName(message.rid);
-    var response;
-    // if 
-    const inpmsg = message.msg.substr(BOTNAME.length + 1);
-    if (inpmsg in respmap) {
-      response = respmap[inpmsg];
-    } else {
-      response = message.u.username + ', how can ' + BOTNAME + ' help you with ' + message.msg.substr(BOTNAME.length + 1);
-    }
-    const sentmsg = await driver.sendToRoom(response, roomname);
+
+    const response = respmap[message.msg] ?? `${message.u.username}, sorry didnt get that`;
+    await driver.sendToRoom(response, roomname);
   }
 }
 
--- reply.js
+++ reply.js
@@ -1,9 +1,8 @@
 const respmap = {
-    "mls" : "I can gladly access the MLS system for you",
-    "tres figueiras" : "Neigbourhood selected : Tres Figueiras",
-    "2 bdrm" :  "There are 201 two bedroom apartment listings in this region",
-    "1 bdrm": "There are 102 one bedroom apartment listings in this region",
-    "studio": "There are 33 studio apartment listings in this region"
-  };
-  
-  module.exports = respmap;
+  "hi" : "hey",
+  "u da bot" : "no, YOU da bot",
+  "no u da bot" : "Come'on - YOU DA BOT!!",
+  "I give up" : "ok. silly human :rolleyes:"
+};
+
+module.exports = respmap;
