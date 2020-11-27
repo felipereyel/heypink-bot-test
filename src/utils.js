801e365e82fb7b20b7d7d5ae7e25836e9531fad7
felipereyel <felipe.reyel@gmail.com>
2020-11-27 13:29:44 -0300

    fix hour

--- src/utils.js
+++ src/utils.js
@@ -9,7 +9,7 @@ const iMatch = (str, exp) => str ? str.match(new RegExp(exp, "i")) : false;
 const availabilityChecker = (weeklyvailability) => {
     // "09:00/17:00", "09:00/17:00, sex@09:00/15:00" or "09:00/17:00, qui@09:00/15:00, sex@09:00/15:00"
     const dayOfWeek = daysOfWeek[moment().tz("America/Sao_Paulo").day()];
-    const hour = moment().tz("America/Sao_Paulo").format("hh:mm");
+    const hour = moment().tz("America/Sao_Paulo").format("HH:mm");
 
     const [ defaultHours, ...rest ] = weeklyvailability.split(",").map(h => h.trim());
     const specificHours = rest.filter(h => h.includes(dayOfWeek)).map(h => h.replace(dayOfWeek, "").replace("@", "").trim())[0];
