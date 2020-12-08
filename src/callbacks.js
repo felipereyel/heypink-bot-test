1f07a49e18ce87ef07bdf34bc15991c01229e785
felipereyel <felipe.reyel@gmail.com>
2020-12-07 22:41:35 -0300

    b.respond -> console.log

--- src/callbacks.js
+++ src/callbacks.js
@@ -7,34 +7,34 @@ const optionReplyBuilder = (response) => response.child_messages.map(r => `*${r.
 
 const optionsCallbackBuilder = (response) => (b) => {
     b.bot.memory.set(b.message.user.room.id, { id: response.id, created_at: Date.now() });
-    b.respond(`${response.message}\n\nDigite somente a primeira parte da opção desejada:\n${optionReplyBuilder(response)}`);
+    console.log(`${response.message}\n\nDigite somente a primeira parte da opção desejada:\n${optionReplyBuilder(response)}`);
 };
 
 const endCallbackBuilder = (response) => (b) => {
     b.bot.memory.unset(b.message.user.room.id);
-    b.respond(response.message);
+    console.log(response.message);
 };
 
 const redirectCallbackBuilder = (response) => (b) => {
     if (availabilityChecker(response.event_details.availability)) {
         b.bot.memory.set(b.message.user.room.id, { id: response.id, created_at: Date.now() });
-        b.respond(response.message);
-        bot.adapters.message.api.post('rooms.setCustomFields', {
-            rid: b.user.room.id,
-            data: { departmentQueue: response.event_details.redirect_to }
-        });
+        console.log(response.message);
+        // bot.adapters.message.api.post('rooms.setCustomFields', {
+        //     rid: b.user.room.id,
+        //     data: { departmentQueue: response.event_details.redirect_to }
+        // });
     } else {
         b.bot.memory.unset(b.message.user.room.id);
-        b.respond(response.event_details.unavailable_text);
+        console.log(response.event_details.unavailable_text);
     }
 };
 
 const optionsFallbackBuilder = (response) => (b) => {
-    b.respond(`${response.event_details.invalid_option_text}\n\nDigite somente a *primeira parte* da opção desejada:\n${optionReplyBuilder(response)}`);
+    console.log(`${response.event_details.invalid_option_text}\n\nDigite somente a *primeira parte* da opção desejada:\n${optionReplyBuilder(response)}`);
 };
 
 const redirectFallbackBuilder = (response) => (b) => {
-    b.respond(`Aguarde um pouco, em breve alguém vai te atender`);
+    console.log(`Aguarde um pouco, em breve alguém vai te atender`);
 };
 
 exports.resetCallbackBuilder = resetCallbackBuilder;
